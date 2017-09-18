cfa46f18d197775d0d0914d6e87c99fcd46f9a89
unknown <Eric.Andruszko@iam.local>
2017-09-18 16:15:22 -0500

    First up
    first commit, submitted week 2, using files from week 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    int playerHealth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("called from start.");
+        playerHealth = 10;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Debug.Log("You're a nerd.");
+
+	}
+}
