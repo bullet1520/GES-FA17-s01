3634bc5aa19747abb939d0c5ba93fd70c909f274
unknown <Eric.Andruszko@iam.local>
2017-10-09 17:20:30 -0500

    Put in TILED assets
    yep

---
+++ Assets/GenericForest.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="GenericForest" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="4aygb25m.bmp" width="272" height="128"/>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="5" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
